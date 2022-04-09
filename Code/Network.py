244c6ce135cfb65c861c69403cb0c70de6167217
MAmJ4 <57817696+MAmJ4@users.noreply.github.com>
2022-04-09 13:04:35 +0100

    Organising Folders, Data Libraries
    Organised prototypes into separate folders, wrote feedforward into main network class, rewrote and improved my data library.

--- Code/Network.py
+++ Code/Network.py
@@ -1,18 +1,55 @@
 import numpy as np
+import data
+from data import Data
 
 sigmoid = lambda x : 1.0/(1.0+np.exp(-x))
 
-class Data ():
 
 class Network (object):
-	def __init__ (self, structure)
-		self.size = len(structure)
-		self.structure = structure
-		self.weights = []
-		self.biases = []
-		for x in range (0, (size - 1)):
+	def __init__ (self, structure):
+		d = Data ()
+		self.size = len(structure) # set size equal to number of layers
+		self.structure = structure # set structure (n of neurons) to match input
+		
+		self.weights = [] # declare weight array
+		self.biases = [] # declare bias array
+		for x in range (0, (self.size - 1)):
+			# initialise weights and biases with random values
 			self.weights.append(np.asarray(np.random.rand(structure[x+1],structure[x])))
 			self.biases.append(np.asarray(np.random.rand(structure[x+1],1)))
-		self.activations =[]
-		for x in range (0, size):
+		
+		self.activations =[] # declare activations
+		for x in range (0, self.size):
+			# initialise activations with zeros
 			self.activations.append(np.zeros((structure[x],1)))
+
+	def feedforward (self, data):
+		Z = (data[0]-min(d.getZ()))/(max(d.getZ())-min(d.getZ()))
+		N = (data[1]-min(d.getN()))/(max(d.getN())-min(d.getN()))
+		A = (data[2]-min(d.getA()))/(max(d.getA())-min(d.getA()))
+		Q = (data[3]-min(d.getQ()))/(max(d.getQ())-min(d.getQ()))
+		Zd = (data[4]-min(d.getZDist()))/(max(d.getZDist())-min(d.getZDist()))
+		Nd = (data[5]-min(d.getNDist()))/(max(d.getNDist())-min(d.getNDist()))
+	
+		self.activations [0] = np.array([[Z], [N], [A], [Q], [Zd], [Nd]]) # use scaled inputs as initial activations
+
+		for x in range (0, self.size - 2):
+			a = self.activations [x]
+			mult = np.array([np.matmul(self.weights[x], a)])
+			mult = np.reshape(mult, (self.structure[x+1],1))
+			self.activations [x+1] = sigmoid(mult + self.biases[x])
+
+		af = np.array([])
+		mult = np.array([np.matmul(self.weights[self.size - 2], self.activations [self.size - 2])])
+		mult = np.reshape (mult, (self.structure[self.size - 1],1))
+		af = mult + self.biases[self.size - 2]
+
+		return af
+
+
+
+
+d = Data()
+net = Network ([6,25,30,30,25,1])
+print(net.feedforward ((d.getIsotope()[123])))
+#print (Network.ZArray)
---
+++ Code/data.py
@@ -0,0 +1,84 @@
+import csv as csv
+import numpy as np
+
+class Data ():
+
+	def __init__ (self):
+		self.data = []
+		with open("Database.csv") as database:
+			csvreader = csv.reader (database)
+			for row in csvreader:
+				self.data.append([row[0],int(row[1]),int(row[2]),int(row[3]),float(row[4]),float(row[5]),float(row[6])])
+		
+		# Format: Element, Z, N, A, Q, T12, ELDM
+		for isotope in self.data:
+
+			Zdist = min([abs(isotope[1]-2), abs(isotope[1]-8), abs(isotope[1]-20), 
+				abs(isotope[1]-28), abs(isotope[1]-50), abs(isotope[1]-82), abs(isotope[1]-126)])
+			isotope.insert (2, Zdist)
+
+			Ndist = min([abs(isotope[3]-2), abs(isotope[3]-8), abs(isotope[3]-20), 
+				abs(isotope[3]-28), abs(isotope[3]-50), abs(isotope[3]-82), abs(isotope[3]-84), abs(isotope[3]-126)])
+			isotope.insert (4, Ndist)
+		
+		# Format: Element, Z, ZDist, N, NDist, A, Q, T12, ELDM
+
+
+	def getZ (self):
+		Z = []
+		for x in self.data:
+			Z.append(x[1])
+		return Z
+
+	def getZDist (self):
+		ZDist = []
+		for x in self.data:
+			ZDist.append(x[2])
+		return ZDist
+
+	def getN (self):
+		N = []
+		for x in self.data:
+			N.append (x[3])
+		return N
+
+	def getNDist (self):
+		NDist = []
+		for x in self.data:
+			NDist.append(x[4])
+		return NDist
+
+	def getA (self):
+		A = []
+		for x in self.data:
+			A.append (x[5])
+		return A
+
+	def getQ (self):
+		Q = []
+		for x in self.data:
+			Q.append (x[6])
+		return Q
+
+	def getHL (self):
+		HL = []
+		for x in self.data:
+			HL.append (x[7])
+		return HL
+
+	def getModel (self):
+		Model = []
+		for x in self.data:
+			Model.append (x[8])
+		return Model
+
+	def getIsotope (self):
+		Isotope = []
+		for x in self.data:
+			Isotope.append ([x[1], x[3], x[5], x[6], x[2], x[4]])
+		return Isotope
+
+
+
+d = Data()
+print(d.data[0])
